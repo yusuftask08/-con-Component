7e0b7d69330948718f2c16dfaeee2018f882f30c
yusuftask08 <yusuftaskiran1@egmail.com>
2023-01-22 10:31:13 +0300

    init app

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <BaseIcon name="success" width="45" height="40" />
+    <BaseIcon name="error" width="45" height="40" />
+    <BaseIcon name="info" width="45" height="40" />
+  </div>
+</template>
+
+<script>
+import BaseIcon from "./components/BaseIcon.vue";
+
+export default {
+  name: "App",
+  components: {
+    BaseIcon,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <component :is="iconComponent" />
+</template>
+
+<script>
+const icons = {
+  error: require("./Icons/ErrorIcon.vue").default,
+  success: require("./Icons/SuccessIcon.vue").default,
+  info: require("./Icons/InfoIcon.vue").default,
+};
+
+export default {
+  name: "BaseIcon",
+  props: {
+    name: {
+      type: String,
+      required: true,
+      validator(value) {
+        return Object.prototype.hasOwnProperty.call(value, icons);
+      },
+    },
+  },
+  computed: {
+    iconComponent() {
+      return icons[this.name];
+    },
+  },
+};
+</script>
